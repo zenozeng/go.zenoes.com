bdebe54955628d65a52d4ff866c2150f749d2620
Zeno Zeng <zenoofzeng@gmail.com>
2014-06-10 21:35:30 +0800

    add map.js, fixes #2, list all items in index, fixes #1

---
+++ map.js
@@ -0,0 +1,9 @@
+var map = {
+    gx: "http://www.zjuitc.com/showarticle.asp?cls=1&artid=939",
+    sh: "http://222.205.42.106/sh.doc",
+    chiyo1: "http://box.myqsc.com/-final1",
+    chiyo2: "http://box.myqsc.com/-final3",
+    chiyo3: "http://box.myqsc.com/-final4",
+    srtp: "http://bksy.zju.edu.cn/redir.php?catalog_id=711213&object_id=973675",
+    wsw: "http://www.zjubiolab.zju.edu.cn/page/coursenews.php?coursenews_courseCatalogId=129"
+};
